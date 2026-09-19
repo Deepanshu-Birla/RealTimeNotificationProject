7f930134b17797f1c90e848d2f71bb551af3b8b1
Deepanshu Birla <dipanshubirla083@gmail.com>
2026-09-19 23:35:48 +0530

    initial commit

---
+++ src/main/java/org/example/realtimenoti/RealTimeNotiApplication.java
@@ -0,0 +1,13 @@
+package org.example.realtimenoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeNotiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeNotiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/realtimenoti/RealTimeNotiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.realtimenoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeNotiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
